c497e5d496f4c40c2eeaa17723f2e96662a110a5
HammadAmeer <hammadameerf@gmail.com>
2023-03-01 00:34:11 +0500

    Initial Commit

---
+++ Question06/Question06/question06.ts
@@ -0,0 +1,5 @@
+// '\t' means four spaces 
+// '\n' used for end line
+let person_name:string='    \tHammad Ameer\n    '   //store name with white spaces.
+console.log('Name Before Stripping : '+person_name);    //display name on console without stripping.
+console.log('Name After Stripping : '+person_name.trim());    //display name on console after stripping.
