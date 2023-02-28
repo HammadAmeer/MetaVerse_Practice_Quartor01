557b9b0a477ffe53f53359aa336b40b95457ae0d
HammadAmeer <hammadameerf@gmail.com>
2023-03-01 01:14:03 +0500

    Initial Commit

---
+++ Question7&8/Question7&8_Code/question7n8.js
@@ -0,0 +1,9 @@
+"use strict";
+// '+' sign is used for addition.
+console.log("2 + 6 = " + (2 + 6)); //display addition of 2 and 6 whose resultant will be 8.
+// '-' sign is used for substraction.
+console.log("12 - 4 = " + (12 - 4)); //display subtraction of 12 and 4 whose resultant will be 8.
+// '*' sign is used for multiplication.
+console.log("2 * 4 = " + (2 * 4)); //display multiplication of 2 and 4 whose resultant will be 8.
+// '/' sign is used for division.
+console.log("16 / 2 = " + (16 / 2)); //display division of 16 and 2 whose resultant will be 8.
