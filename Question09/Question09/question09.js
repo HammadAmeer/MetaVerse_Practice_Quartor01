361a8c7540f8d7c35e5eaed9977661d24fccda29
HammadAmeer <hammadameerf@gmail.com>
2023-03-06 23:24:14 +0500

    Initial Commit

---
+++ Question09/Question09/question09.js
@@ -0,0 +1,3 @@
+"use strict";
+let fav_num = 50; //I store my fav number in variable named as 'fav_num'.
+console.log("I want to earn " + fav_num + " million dollars on each project");
---
+++ Question09/Question09/question09.ts
@@ -0,0 +1,2 @@
+let fav_num:number=50;      //I store my fav number in variable named as 'fav_num'.
+console.log("I want to earn "+fav_num+" million dollars on each project");
